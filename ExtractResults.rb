9bb1e133e58b70cf4a4deba69d6546e2fe3a4e8a
Joshua Ferguson <jwfergus@asu.edu>
2012-05-08 08:35:48 -0700

    Corrected .gitignore to correctly include some needed files (*.original). Copied over the scripts to commit.

---
+++ ExtractResults.rb
@@ -0,0 +1,45 @@
+ 
+  #Compiling results
+  mainDirectory = "/home/bluesim/OpenFOAM/OpenFOAM-1.6.x/tutorials/incompressible/transientSimpleFoam/igccpaper/crac_results/Probes_Crac"
+  Dir.chdir(mainDirectory)
+  
+  resultsFileName = "Main_Data_probes__Results"
+  
+  directories = Dir.entries(Dir.pwd).sort
+  system("touch #{resultsFileName}")
+  mainResultsFile = File.open(resultsFileName,'a')
+  directories.delete_if{|dir| dir == "." || dir == ".." || dir == "ExtractResults.rb"}
+  directories.each{|dir|
+    
+    resultsLineAsString =""
+    Dir.chdir(mainDirectory+"/#{dir.to_s}/20")
+	puts "going into directory: probes2_Results/#{dir.to_s}/20"
+    tFile = File.readlines('T')
+    lineCount = 1
+    tFile.each{|line|
+      lineCount += 1
+      if lineCount <6
+        next
+      end
+      resultsLineAsString +=line.split(" ").last+ ","
+      
+    }
+    
+    Dir.chdir(mainDirectory+"/#{dir.to_s}/21")
+    tFile = File.readlines('T')
+    lineCount = 1
+    tFile.each{|line|
+      lineCount += 1
+      if lineCount <6
+        next
+      end
+      
+      resultsLineAsString +=line.split(" ").last+ ","
+      
+      
+    }
+    n = resultsLineAsString.length - 1
+    resultsLineAsString = resultsLineAsString[0...n]
+    #puts resultsLineAsString
+    mainResultsFile.puts resultsLineAsString
+  }
