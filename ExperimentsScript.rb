27ad1a05243547b343ff92131dc0d3419f9e4956
Joshua Ferguson <jwfergus@asu.edu>
2012-06-17 01:44:11 -0700

    Changed the Chassis labeling to the range Rack1Chassis1...Rack4Chassis6, iterating each number by one. Also, changed the way the solver C file naming was handled by specifying the C file at the beginning of the script only.

--- ExperimentsScript.rb
+++ ExperimentsScript.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
 #
 #  Main Options
@@ -6,12 +6,13 @@
 Q = 1466318.6604
 CRAC_Q = 111111.1111
 firstEndTime = 21
-secondEndTime = 22
+secondEndTime = 90
 mainDirectory = Dir.pwd
+solverFile = 'transientSimpleFoam.C.smallContainer.original'
 openFoamRootDirectory = "/home/bluesim/OpenFOAM/OpenFOAM-1.6.x/"
-ChassisList = ['//____CRAC_____', "//rack1chassis2","//rack1chassis4","//rack1chassis6",'//rack2chassis2','//rack2chassis4','//rack2chassis6','//rack3chassis2','//rack3chassis4',
-'//rack3chassis6','//rack4chassis2','//rack4chassis4','//rack4chassis6','//rack5chassis2','//rack5chassis4','//rack5chassis6','//rack6chassis2','//rack6chassis4',
-'//rack6chassis6','//rack7chassis2','//rack7chassis4','//rack7chassis6','//rack8chassis2','//rack8chassis4','//rack8chassis6']
+ChassisList = ['//____CRAC_____', "//rack1chassis1","//rack1chassis2","//rack1chassis3",'//rack1chassis4','//rack1chassis5','//rack1chassis6','//rack2chassis1','//rack2chassis2',
+'//rack2chassis3','//rack2chassis4','//rack2chassis5','//rack2chassis6','//rack3chassis1','//rack3chassis2','//rack3chassis3','//rack3chassis4','//rack3chassis5',
+'//rack3chassis6','//rack4chassis1','//rack4chassis2','//rack4chassis3','//rack4chassis4','//rack4chassis5','//rack4chassis6']
 
 
 #
@@ -41,12 +42,78 @@ Dir.chdir(mainDirectory)
 	#
 	system("ruby clean.rb #{secondEndTime}")
 
+	############################################################################################
+	#### Running simulation until steady state (20 seconds)
+	############################################################################################
+	
+	#
+	#	Section to handle changing transientSimpleFoam.C file. Setting all outlets to original Temp.
+	#
+	Dir.chdir(openFoamRootDirectory + 'applications/solvers/incompressible/transientSimpleFoam')
+	origTransientSimpleFoamC = File.readlines(solverFile)
+	system("cp #{solverFile} transientSimpleFoam.C")
+
+	#
+	# Remake solver
+	#
+	system("wmake libso ")
+	Dir.chdir(openFoamRootDirectory + 'applications')
+	system("./Allwmake ")
+
+
+	#
+	# Change All Q Values to zero
+	#
+	Dir.chdir(mainDirectory+'/system')
+
+	origSetFieldsDictFile = File.readlines('setFieldsDict.original')
+	setFieldsDictFile = File.open('setFieldsDict', 'w')
+	origSetFieldsDictFile.each{|line|
+	  setFieldsDictFile.puts(line)
+	}  
+	setFieldsDictFile.close
+
+
+	#
+	# Update endtime to 20 (Steady State time)
+	#
+	origControlDict = File.readlines('controlDict.original')
+	setControlDict = File.open('controlDict', 'w')
+	passedFirst = false
+	origControlDict.each{|line|
+	if line.include?( 'endTime')
+	  if passedFirst==false
+		passedFirst = true
+		setControlDict.puts(line)
+	  else
+		setControlDict.puts "endTime\t\t20;"
+		puts
+	  end
+	else
+	  setControlDict.puts(line)
+	end
+
+	}
+	setControlDict.close
+
+
+	#
+	# Run final time series of simulation with final endTime
+	#
+	Dir.chdir(mainDirectory)
+	system("setFields -latestTime ")
+	system("transientSimpleFoam ")
+
+
+	############################################################################################
+	#### Steady State Reached - Beginning First End Time Simulation
+	############################################################################################
 
 	#
 	#	Suppress temperature control in transientSimpleFoam.C (within the solver)
 	#
 	Dir.chdir(openFoamRootDirectory + 'applications/solvers/incompressible/transientSimpleFoam')
-	origTransientSimpleFoamC = File.readlines('transientSimpleFoam.C.original')
+	origTransientSimpleFoamC = File.readlines(solverFile)
 	setTransientSimpleFoamC = File.open('transientSimpleFoam.C', 'w')
 	origTransientSimpleFoamC.each{|line|
 	if line.include?( chassis.to_s)
@@ -124,11 +191,11 @@ Dir.chdir(mainDirectory)
 	############################################################################################
 	
 	#
-	#	Section to handle changing transientSimpleFoam.C file.
+	#	Section to handle changing transientSimpleFoam.C file. Setting all outlets to original Temp.
 	#
 	Dir.chdir(openFoamRootDirectory + 'applications/solvers/incompressible/transientSimpleFoam')
-	origTransientSimpleFoamC = File.readlines('transientSimpleFoam.C.original')
-	system("cp transientSimpleFoam.C.original transientSimpleFoam.C")
+	origTransientSimpleFoamC = File.readlines(solverFile)
+	system("cp #{solverFile} transientSimpleFoam.C")
 
 	#
 	# Remake solver
@@ -197,7 +264,7 @@ Dir.chdir(mainDirectory)
 	directoryFolder.slice!(0)
 	directoryFolder+='__Results'
 	system("mkdir #{directoryFolder} ")
-	system("cp -r Rack* #{directoryFolder} ")
+	system("cp -r Chassis* #{directoryFolder} ")
 	system("cp -r probes2 #{directoryFolder} ")
 
 	#Compiling results
